3a7483a40e6875dad5d4453723a4ec9ddac09d88
Richard <Richard>
2017-07-23 13:33:29 +0000

    phan mem

--- đăng ký microsoft handlab.bat
+++ đăng ký microsoft handlab.bat
@@ -1,4 +1,4 @@
-start https://login.live.com/login.srf?lw=1
-start https://profile.live.com 
-start https://www.microsoft.com/handsonlabs
-start  chrome temp-mail.org
+start chrome https://login.live.com/login.srf?lw=1
+start chrome https://profile.live.com 
+start chrome https://www.microsoft.com/handsonlabs
+start chrome temp-mail.org
