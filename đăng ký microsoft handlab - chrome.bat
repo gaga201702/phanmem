0aa5586c325ae572420ab76c2dcfa0b0aadb28e1
Richard <Richard>
2017-07-24 06:00:03 +0000

    phanmem

---
+++ đăng ký microsoft handlab - chrome.bat
@@ -0,0 +1,4 @@
+start chrome https://login.live.com/login.srf?lw=1
+start chrome https://profile.live.com 
+start chrome https://www.microsoft.com/handsonlabs
+start chrome temp-mail.org
---
+++ đăng ký microsoft handlab - default.bat
@@ -0,0 +1,4 @@
+start  https://login.live.com/login.srf?lw=1
+start  https://profile.live.com 
+start  https://www.microsoft.com/handsonlabs
+start  https://temp-mail.org
